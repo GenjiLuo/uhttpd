c68cdc77f8565b6672d7b3199082d9b8f21804ee
Robbie Cao <robbie.cao@gmail.com>
2016-09-07 01:25:06 +0800

    Implement url /get and /set with parameters
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- uhttpd.c
+++ uhttpd.c
@@ -10,6 +10,9 @@
 
 #define PORT 8888
 
+static char value_to_set[512];
+static char page_to_answer[2048];
+
 int print_out_key (void *cls, enum MHD_ValueKind kind,
         const char *key, const char *value)
 {
@@ -23,21 +26,45 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
         const char *upload_data,
         size_t *upload_data_size, void **con_cls)
 {
-    const char *page  = "<html><body>Hello, WhosKPW3!</body></html>";
+    const char *page_default = "<html><body>Hello, WhosKPW3!</body></html>";
+    const char *page = page_default;
     struct MHD_Response *response;
-    int ret;
+    int pos = 0;
+    int ret = 0;
 
-    response = MHD_create_response_from_buffer (strlen (page),
-            (void*) page, MHD_RESPMEM_PERSISTENT);
-    printf ("New %s request for %s using version %s\n", method, url, version);
-    printf ("++++++\n");
-
+    printf ("-- Header --\n");
     MHD_get_connection_values (connection, MHD_HEADER_KIND, &print_out_key, NULL);
 
-    printf ("++++++\n");
+    printf ("-- Argument --\n");
     MHD_get_connection_values (connection, MHD_GET_ARGUMENT_KIND, &print_out_key, NULL);
 
-    //printf ("%s - %s\n", "key", MHD_lookup_connection_value (connection, MHD_GET_ARGUMENT_KIND, "key"));
+    // Response according to WhosKPW3 requirement
+    if (!strcmp(url, "/get")) {
+        if (MHD_lookup_connection_value (connection, MHD_GET_ARGUMENT_KIND, "key")) {
+            pos = snprintf(page_to_answer, sizeof(page_to_answer) - pos, "<html><body>");
+            pos = snprintf(page_to_answer + strlen(page_to_answer), sizeof(page_to_answer) - strlen(page_to_answer), "value=%s", value_to_set);
+            pos = snprintf(page_to_answer + strlen(page_to_answer), sizeof(page_to_answer) - strlen(page_to_answer), "</html></body>");
+            page = page_to_answer;
+        } else {
+            page = "<html><body>Invalid parameters! Expect: /get?key=xxx</body></html>";
+        }
+    } else if (!strcmp(url, "/set")) {
+        if (MHD_lookup_connection_value (connection, MHD_GET_ARGUMENT_KIND, "key") &&
+                MHD_lookup_connection_value (connection, MHD_GET_ARGUMENT_KIND, "value")) {
+            page = "<html><body>OK</body></html>";
+            snprintf(value_to_set, sizeof(value_to_set), "%s",
+                    MHD_lookup_connection_value (connection, MHD_GET_ARGUMENT_KIND, "value"));
+            printf ("Set value=%s\n", value_to_set);
+        } else {
+            page = "<html><body>Invalid parameters!Expect: /set?key=xxx&value=yyy</body></html>";
+        }
+    } else {
+        page = page_default;
+    }
+
+    printf ("New %s request for %s using version %s\n", method, url, version);
+    response = MHD_create_response_from_buffer (strlen (page),
+            (void*) page, MHD_RESPMEM_PERSISTENT);
 
     ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
     MHD_destroy_response (response);
@@ -48,12 +75,18 @@ return ret;
 int main ()
 {
     struct MHD_Daemon *daemon;
+
+    snprintf(value_to_set, sizeof(value_to_set), "NOT_SET");
+
     daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
             &answer_to_connection, NULL, MHD_OPTION_END);
+
     if (NULL == daemon) {
         return 1;
     }
-    getchar ();
+
+    (void) getchar ();
     MHD_stop_daemon (daemon);
+
     return 0;
 }
