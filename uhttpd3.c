2272614cb057a724b17f8827760cdaba15561b0e
Robbie Cao <robbie.cao@gmail.com>
2016-09-11 18:27:50 +0800

    Implement get/set value by key with redis async
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- uhttpd3.c
+++ uhttpd3.c
@@ -88,63 +88,83 @@ default_cb(evhtp_request_t * req, void * arg)
 }
 
 static void
+redis_get_cb(redisAsyncContext * redis, void * redis_reply, void * arg)
+{
+    redisReply      * reply = redis_reply;
+    evhtp_request_t * req   = arg;
+
+    if (reply == NULL || reply->type != REDIS_REPLY_STRING) {
+        evbuffer_add_printf(req->buffer_out, "Redis failure\n");
+    } else {
+        evbuffer_add_printf(req->buffer_out,
+                "value=%s\n", reply->str);
+    }
+
+    evhtp_send_reply(req, EVHTP_RES_OK);
+    evhtp_request_resume(req);
+
+    (void) redis;
+}
+
+static void
+redis_set_cb(redisAsyncContext * redis, void * redis_reply, void * arg)
+{
+    redisReply      * reply = redis_reply;
+    evhtp_request_t * req   = arg;
+
+    if (reply == NULL || reply->type != REDIS_REPLY_STATUS || strcasecmp(reply->str,"OK")) {
+        evbuffer_add_printf(req->buffer_out, "Redis failure\n");
+    } else {
+        evbuffer_add_printf(req->buffer_out, "OK\n");
+    }
+
+    evhtp_send_reply(req, EVHTP_RES_OK);
+    evhtp_request_resume(req);
+
+    (void) redis;
+}
+
+static void
 set_request_cb(evhtp_request_t * req, void * arg)
 {
+    struct app         * app;
+    evthr_t            * thread;
+
     dump_request(req);
-    evbuffer_add_printf(req->buffer_out, "%s", arg ? (const char *)arg : "Null");
-    evhtp_send_reply(req, EVHTP_RES_OK);
+
+    thread = get_request_thr(req);
+    app    = (struct app *)evthr_get_aux(thread);
+
+    redisAsyncCommand(app->redis,
+            redis_set_cb,
+            (void *)req,
+            "SET %s %s",
+            evhtp_kv_find(req->uri->query, "key"),
+            evhtp_kv_find(req->uri->query, "value")
+            );
+
+    (void) arg;
 }
 
 /* Callback used for the /get URI */
 static void
 get_request_cb(evhtp_request_t * req, void * arg)
 {
-    dump_request(req);
-    evbuffer_add_printf(req->buffer_out, "%s", arg ? (const char *)arg : "Null");
-    evhtp_send_reply(req, EVHTP_RES_OK);
-}
-
-
-void
-app_process_request(evhtp_request_t * req, void * arg)
-{
-    struct sockaddr_in * sin;
-    struct app_parent  * app_parent;
     struct app         * app;
     evthr_t            * thread;
-    evhtp_connection_t * conn;
-    char                 tmp[1024];
 
-    thread = get_request_thr(req);
-    printf("o Thread(%p)\n", thread);
     dump_request(req);
 
-    conn   = evhtp_request_get_connection(req);
+    thread = get_request_thr(req);
     app    = (struct app *)evthr_get_aux(thread);
-    sin    = (struct sockaddr_in *)conn->saddr;
 
-    evutil_inet_ntop(AF_INET, &sin->sin_addr, tmp, sizeof(tmp));
+    redisAsyncCommand(app->redis,
+            redis_get_cb,
+            (void *)req,
+            "GET %s",
+            evhtp_kv_find(req->uri->query, "key"));
 
-#if 0
-    /* increment a global counter of hits on redis */
-    redisAsyncCommand(app->redis, redis_global_incr_cb,
-            (void *)req, "INCR requests:total");
-
-    /* increment a counter for hits from this source address on redis */
-    redisAsyncCommand(app->redis, redis_srcaddr_incr_cb,
-            (void *)req, "INCR requests:ip:%s", tmp);
-
-    /* add the source port of this req to a source-specific set */
-    redisAsyncCommand(app->redis, redis_set_srcport_cb, (void *)req,
-            "SADD requests:ip:%s:ports %d", tmp, ntohs(sin->sin_port));
-
-    /* get all of the ports this source address has used */
-    redisAsyncCommand(app->redis, redis_get_srcport_cb, (void *)req,
-            "SMEMBERS requests:ip:%s:ports", tmp);
-#endif
-
-    /* pause the req processing */
-    evhtp_request_pause(req);
+    (void) arg;
 }
 
 void
@@ -163,6 +183,8 @@ app_init_thread(evhtp_t * htp, evthr_t * thread, void * arg)
     redisLibeventAttach(app->redis, app->evbase);
 
     evthr_set_aux(thread, app);
+
+    (void) htp;
 }
 
 
@@ -184,13 +206,10 @@ main(int argc, char ** argv)
     app_p->redis_host = REDIS_HOST;
     app_p->redis_port = REDIS_PORT;
 
-    evhtp_set_gencb(htp, app_process_request, NULL);
-    evhtp_set_cb(htp, "/", app_process_request, "Hello WhosKPW3\n");
-#if 1
-    //evhtp_set_cb(htp, "/", default_cb, "Hello WhosKPW3\n");
+    evhtp_set_gencb(htp, default_cb, NULL);
+    evhtp_set_cb(htp, "/", default_cb, "Hello WhosKPW3\n");
     evhtp_set_cb(htp, "/set", set_request_cb, "Set\n");
     evhtp_set_cb(htp, "/get", get_request_cb, "Get\n");
-#endif
 
 #ifndef EVHTP_DISABLE_EVTHR
     evhtp_use_threads(htp, app_init_thread, 4, app_p);
