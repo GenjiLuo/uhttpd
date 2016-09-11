ad6664fa137d300edbf002e9c92ec03b64405b01
Robbie Cao <robbie.cao@gmail.com>
2016-09-11 23:15:02 +0800

    Add script for setup envrionment in one shot command
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# basic
+echo "Install basic tools..."
+sudo apt-get -y install build-essential wget cmake tcl8.5
+
+# libmicrohttpd
+
+echo "Install libmicrohttpd"
+sudo apt-get -y install libmicrohttpd-dev
+
+# libevent
+
+echo "Install libevent"
+sudo apt-get -y install libevent-dev
+
+# libevhtp
+
+echo "Install libevthp"
+mkdir tmp && pushd tmp
+wget https://github.com/ellzey/libevhtp/archive/1.2.9.tar.gz
+tar zxvf 1.2.9.tar.gz
+cd libevhtp-1.2.9
+mkdir build && cd build
+cmake .. -DEVHTP_BUILD_SHARED:STRING=ON
+make
+sudo make install
+sudo ldconfig
+popd
+
+# hiredis
+
+echo "Install hiredis"
+mkdir tmp && pushd tmp
+wget https://github.com/redis/hiredis/archive/v0.13.3.tar.gz
+tar zxvf v0.13.3.tar.gz
+cd hiredis-0.13.3
+make
+sudo make install
+sudo ldconfig
+popd
+
+# redis
+
+echo "Install redis"
+mkdir tmp && pushd tmp
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
+./install_server.sh
+popd
+
+sudo service redis_6379 start
+
+rm -fr tmp
+
+echo "Setup done!"
